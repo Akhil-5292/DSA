793197b5fc80bf0640d4eaae2c6d2417b8371513
Akhil-5292 <aps.ca9@gmail.com>
2023-03-23 11:22:17 +0530

    wordFrequency

---
+++ wordFrequency.js
@@ -0,0 +1,16 @@
+function WordFrequency(str) {
+    const words = str.toLowerCase().replace(",", "").split(" ");
+    const frequencyContainer = new Map();
+  
+    for (let word of words) {
+      frequencyContainer.set(word, (frequencyContainer.get(word) || 0) + 1);
+    }
+  
+    for (let [key, value] of frequencyContainer) {
+      console.log(`${key} :- ${value}`);
+    }
+  }
+  
+  const str =
+    "Success is not the key of Happiness, Happiness is the key of success";
+  WordFrequency(str);
