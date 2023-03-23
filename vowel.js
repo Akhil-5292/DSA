ea3570fbcf9663458f4278dc305b7016908e04d4
Akhil-5292 <aps.ca9@gmail.com>
2023-03-23 23:09:04 +0530

    vowel

---
+++ vowel.js
@@ -0,0 +1,22 @@
+var longestBeautifulSubstring = function(word) {
+    var ans=0,n=word.length,count=1,len=1
+    for(let i=1;i<n;i++){
+        if(word[i-1]<=word[i]){//true
+            len++;
+            if(word[i-1]!=word[i]){
+                count++
+            }
+        }
+        else{
+            count=1
+            len=1
+        }
+        if(count==5){
+            ans=Math.max(ans,len)
+        }
+    }
+    return ans;
+};
+
+const word='aeiaaioaaaaeiiiiouuuooaauuaeiu'
+console.log(longestBeautifulSubstring(word))
