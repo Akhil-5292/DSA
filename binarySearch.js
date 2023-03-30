50803261b6b0e50d6d5f0259b934a9c3c21f45ca
Akhil-5292 <aps.ca9@gmail.com>
2023-03-30 18:28:18 +0530

    binarySearch

---
+++ binarySearch.js
@@ -0,0 +1,24 @@
+function searchInsert(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return left;
+}
+
+let nums=[2,5,4,9,7,6,3]
+let target =4
+
+
+console.log(searchInsert(nums,target));
