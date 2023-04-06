8313744736b87fe0ea95c1afeac0d5164720de5d
Akhil-5292 <aps.ca9@gmail.com>
2023-04-06 21:12:35 +0530

    insertionSort

---
+++ inserstionSort.js
@@ -0,0 +1,15 @@
+function insertionSort(arr){
+    for(let i=1; i<arr.length; i++){
+        let currVal=arr[i]
+        let j
+
+        for(let j=i-1; j>=0; j--){
+            arr[j+1]=arr[j]
+        }
+         arr[j+1]=currVal
+    }
+    return arr
+}
+
+let arr=[2,1,3,7,5]
+console.log(insertionSort(arr));
